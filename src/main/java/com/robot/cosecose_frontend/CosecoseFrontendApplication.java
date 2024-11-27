0cebee8fff7e39cf0e7402c0ef5a9fc497b176fb
TreeBomb-01 <htb010630@gmail.com>
2024-11-27 13:13:53 +0900

    cosecose_frontend초기파일

---
+++ src/main/java/com/robot/cosecose_frontend/CosecoseFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.robot.cosecose_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosecoseFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosecoseFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robot/cosecose_frontend/CosecoseFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robot.cosecose_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosecoseFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
